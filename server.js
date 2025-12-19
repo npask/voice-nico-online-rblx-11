f7fffd93142b9822960b7ce26a092ec0dcbb9425
npask <87610771+npask@users.noreply.github.com>
2025-12-19 21:15:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
