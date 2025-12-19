26d49eace19fcbf3203a46b80dcaa98c90129050
npask <87610771+npask@users.noreply.github.com>
2025-12-19 20:58:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const io = new Server(server, {
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 let players = {}; // speichert aktuelle Spieler-Positionen {id: {x,y,z,name}}
 
